ab29aa50eae8c76bcc8e880d4b6c901eb4b481cb
HANU0502 <padmanabham0502@gmail.com>
2020-08-20 00:20:03 +0530

    login and logout

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://hanu:12345@Cluster0.ras5p.mongodb.net/Cluster0?retryWrites=true&w=majority'
+
+module.exports = {
+    mongoURI: dbPassword
+};
---
+++ public/js/type.js
@@ -0,0 +1,33 @@
+//type A
+if(document.querySelector("body > div > div.jumbotron.jumbotron-fluid > div > p.text-dark").innerText=="A"){
+    document.querySelector(".img1").style.display="inline";
+}
+else{
+     document.querySelector(".img1").style.display="none";
+}
+//type B
+if(document.querySelector("body > div > div.jumbotron.jumbotron-fluid > div > p.text-dark").innerText=="B"){
+    document.querySelector("#img2").style.display="inline";
+}
+else{
+     document.querySelector("#img2").style.display="none";
+}
+if(document.querySelector("body > div > div.jumbotron.jumbotron-fluid > div > p.text-dark").innerText=="B"){
+    document.querySelector("#img3").style.display="inline";
+}
+else{
+     document.querySelector("#img3").style.display="none";
+}
+//type C
+if(document.querySelector("body > div > div.jumbotron.jumbotron-fluid > div > p.text-dark").innerText=="C"){
+    document.querySelector("#img4").style.display="inline";
+}
+else{
+     document.querySelector("#img4").style.display="none";
+}
+if(document.querySelector("body > div > div.jumbotron.jumbotron-fluid > div > p.text-dark").innerText=="C"){
+    document.querySelector("#img5").style.display="inline";
+}
+else{
+     document.querySelector("#img5").style.display="none";
+}
